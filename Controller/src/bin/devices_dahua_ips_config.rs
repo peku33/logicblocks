9b3f974c00a5bc504b97fb9e1c759ea7b03d9cd2
Paweł Kubrak <peku33@gmail.com>
2019-12-02 21:49:04 +0100

    Added devices_dahua_ips_config.rs

---
+++ Controller/src/bin/devices_dahua_ips_config.rs
@@ -0,0 +1,43 @@
+extern crate logicblocks_controller;
+
+use logicblocks_controller::devices::dahua::ipc::api::{Client, SaneDefaultsConfig};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .filter_module("logicblocks_controller", log::LevelFilter::Trace)
+        .init();
+
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 6 {
+        eprintln!(
+            "Usage: <host> <admin_password> <device_name> <shared_user_password> [video_overlay]"
+        );
+    }
+
+    let host = args[1].parse()?;
+    let admin_password = args[2].to_owned();
+
+    let device_name = args[3].to_owned();
+    let shared_user_password = args[4].to_owned();
+    let video_overlay = match args[5].as_str() {
+        "" => None,
+        video_overlay => Some(video_overlay.to_owned()),
+    };
+
+    let client = Client::new(host, admin_password);
+
+    println!("Starting config...");
+    client
+        .sane_defaults(&SaneDefaultsConfig {
+            device_name,
+            shared_user_password,
+            video_overlay,
+        })
+        .await?;
+    println!("Finishing config...");
+
+    return Ok(());
+}
