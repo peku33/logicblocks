d1cc16889123dfd4e5eec4bb6aa04da858f3e8a3
Paweł Kubrak <peku33@gmail.com>
2019-12-02 21:36:49 +0100

    Added global lib

---
+++ Controller/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![feature(async_closure)]
+#![feature(option_expect_none)]
+#![feature(raw_vec_internals)]
+#![feature(trait_alias)]
+#![feature(try_blocks)]
+#![feature(drain_filter)]
+#![warn(clippy::all)]
+
+pub mod devices;
+pub mod util;
+pub mod web;
