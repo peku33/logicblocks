3724af337e7c615398d5320ddf0b3fff17ae17ce
Paweł Kubrak <peku33@gmail.com>
2019-12-02 21:10:53 +0100

    Added utils module

---
+++ Controller/src/util/mod.rs
@@ -0,0 +1,7 @@
+pub mod bus2;
+pub mod ref_mut_async;
+
+// https://stackoverflow.com/questions/50547766/how-can-i-get-impl-trait-to-use-the-appropriate-lifetime-for-a-mutable-reference
+pub trait Captures<'a> {}
+impl<'a, T: ?Sized> Captures<'a> for T {
+}
