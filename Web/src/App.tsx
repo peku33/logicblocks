e299b027a2597a0cfa529a5fe05678aa4f711003
Paweł Kubrak <peku33@gmail.com>
2019-12-09 01:01:27 +0100

    Web init

---
+++ Web/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Body from "./root/Body";
+
+const App: React.FC = () => {
+  return (<p>App</p>);
+};
+
+export default App;
