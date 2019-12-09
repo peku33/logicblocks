62059f2f8c9f94cc56ae9d316331edceeaa72a94
Paweł Kubrak <peku33@gmail.com>
2019-12-09 23:52:46 +0100

    Tied up GUI

--- Web/src/root/Body.tsx
+++ Web/src/root/Body.tsx
@@ -9,8 +9,15 @@ const Body: React.FC = () => {
   return (
     <div>
       <Menu>
+        <SemanticUiReactMenuItemRouter path="/device_pool" text="Devices" />
       </Menu>
       <Switch>
+        <Route path="/device_pool">
+          <DevicePool />
+        </Route>
+        <Route path="/" exact>
+          <Redirect to="/device_pool" />
+        </Route>
         <Route path="*">
           <Error404 />
         </Route>
