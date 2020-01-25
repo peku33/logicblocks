eb11b2466cf94bc246cc49ed6030a73c790a1b7f
Paweł Kubrak <peku33@gmail.com>
2020-01-25 22:15:52 +0100

    Fixed stubs

--- Controller/src/devices/logicblocks/houseblocks_v1/master.rs
+++ Controller/src/devices/logicblocks/houseblocks_v1/master.rs
@@ -3,3 +3,28 @@ pub use super::master_linux::*;
 
 #[cfg(not(target_os = "linux"))]
 pub use super::master_stub::*;
+
+use std::ffi;
+use std::fmt;
+use std::fmt::{Debug, Display};
+
+#[derive(Hash, PartialEq, Eq, Clone, Debug)]
+pub struct MasterDescriptor {
+    pub vid: u16,
+    pub pid: u16,
+    pub serial_number: ffi::CString,
+}
+impl Display for MasterDescriptor {
+    fn fmt(
+        &self,
+        f: &mut fmt::Formatter<'_>,
+    ) -> fmt::Result {
+        return write!(
+            f,
+            "{:04X}:{:04X}:{}",
+            self.vid,
+            self.pid,
+            self.serial_number.to_string_lossy()
+        );
+    }
+}
--- Controller/src/devices/logicblocks/houseblocks_v1/master_linux.rs
+++ Controller/src/devices/logicblocks/houseblocks_v1/master_linux.rs
@@ -1,14 +1,15 @@
 #![cfg(target_os = "linux")]
 
 use super::common::{Address, AddressDeviceType, AddressSerial, Frame, Payload};
+use super::master::MasterDescriptor;
 use failure::{err_msg, format_err, Error};
-use fmt::{Debug, Display};
 use futures::channel::oneshot;
 use scopeguard::defer;
 use std::cell::RefCell;
 use std::convert::TryInto;
 use std::ffi;
 use std::fmt;
+use std::fmt::{Debug, Display};
 use std::mem::MaybeUninit;
 use std::ptr;
 use std::sync::mpsc;
@@ -197,27 +198,6 @@ impl MasterContext {
 impl Drop for MasterContext {
     fn drop(&mut self) -> () {
         unsafe { libftdi1_sys::ftdi_free(self.ftdi_context) }
-    }
-}
-
-#[derive(Hash, PartialEq, Eq, Clone, Debug)]
-pub struct MasterDescriptor {
-    pub vid: u16,
-    pub pid: u16,
-    pub serial_number: ffi::CString,
-}
-impl Display for MasterDescriptor {
-    fn fmt(
-        &self,
-        f: &mut fmt::Formatter<'_>,
-    ) -> fmt::Result {
-        return write!(
-            f,
-            "{:04X}:{:04X}:{}",
-            self.vid,
-            self.pid,
-            self.serial_number.to_string_lossy()
-        );
     }
 }
 
