ae1dce79ca99c10701b918b86eabaae844d1a7f1
Paweł Kubrak <peku33@gmail.com>
2020-02-22 01:37:00 +0100

    Added sqlite module

---
+++ Controller/src/application/sqlite.rs
@@ -0,0 +1,67 @@
+use super::fs::Fs;
+use super::modules::{ModuleFactory, ModuleFactoryTrait, ModuleHandle, ModuleTrait};
+use crate::util::sqlite_async::SqliteAsync;
+use failure::Error;
+use futures::future::{BoxFuture, Future, FutureExt};
+use rusqlite::{Connection, Transaction};
+
+pub struct Sqlite {
+    sqlite_async: SqliteAsync,
+}
+impl Sqlite {
+    pub fn new(fs: ModuleHandle<Fs>) -> Self {
+        let sqlite_file = fs.persistent_data_directory().join("LogicBlocks.sqlite");
+
+        let sqlite_connection = rusqlite::Connection::open(sqlite_file).unwrap();
+
+        sqlite_connection
+            .pragma_update(None, "auto_vacuum", &"INCREMENTAL")
+            .unwrap();
+        sqlite_connection
+            .pragma_update(None, "foreign_keys", &true)
+            .unwrap();
+        sqlite_connection
+            .pragma_update(None, "journal_mode", &"WAL")
+            .unwrap();
+        sqlite_connection
+            .pragma_update(None, "synchronous", &"NORMAL")
+            .unwrap();
+
+        let sqlite_async = SqliteAsync::new(sqlite_connection, "SQLite Module".to_owned());
+
+        Self { sqlite_async }
+    }
+
+    pub fn query<F, R>(
+        &self,
+        f: F,
+    ) -> impl Future<Output = R>
+    where
+        F: FnOnce(&Connection) -> R + Send + 'static,
+        R: Send + 'static,
+    {
+        self.sqlite_async.query(f)
+    }
+
+    pub fn transaction<F, R>(
+        &self,
+        f: F,
+    ) -> impl Future<Output = Result<R, Error>>
+    where
+        F: FnOnce(&mut Transaction) -> R + Send + 'static,
+        R: Send + 'static,
+    {
+        self.sqlite_async.transaction(f)
+    }
+}
+impl ModuleTrait for Sqlite {
+}
+impl ModuleFactoryTrait for Sqlite {
+    fn spawn<'mf>(module_factory: &'mf ModuleFactory) -> BoxFuture<'mf, Self> {
+        async move {
+            let fs = module_factory.get::<Fs>().await;
+            Self::new(fs)
+        }
+        .boxed()
+    }
+}
