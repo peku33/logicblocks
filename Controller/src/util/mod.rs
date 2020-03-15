70988c712e974e49476d006bbb4665986b0387f4
Paweł Kubrak <peku33@gmail.com>
2020-03-15 22:15:52 +0100

    Added select_all_empty

--- Controller/src/util/mod.rs
+++ Controller/src/util/mod.rs
@@ -1,5 +1,6 @@
 pub mod borrowed_async;
 pub mod bus2;
+pub mod select_all_empty;
 pub mod sqlite_async;
 
 // https://stackoverflow.com/questions/50547766/how-can-i-get-impl-trait-to-use-the-appropriate-lifetime-for-a-mutable-reference
