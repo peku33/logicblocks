61d49d2abcba79dbdbd54c9af25f0a294ef05652
Paweł Kubrak <peku33@gmail.com>
2020-03-15 22:15:36 +0100

    Replaced ref_mut_async with borrowed_async

--- Controller/src/util/mod.rs
+++ Controller/src/util/mod.rs
@@ -1,5 +1,5 @@
+pub mod borrowed_async;
 pub mod bus2;
-pub mod ref_mut_async;
 pub mod sqlite_async;
 
 // https://stackoverflow.com/questions/50547766/how-can-i-get-impl-trait-to-use-the-appropriate-lifetime-for-a-mutable-reference
