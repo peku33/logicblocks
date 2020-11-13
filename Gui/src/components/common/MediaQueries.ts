bbe85e7a1f312e5d316c8ce3c8a1fe917b1e5f7b
Paweł Kubrak <peku33@gmail.com>
2020-11-13 02:05:41 +0100

    added white color fixed exports

--- Gui/src/components/common/MediaQueries.ts
+++ Gui/src/components/common/MediaQueries.ts
@@ -1,6 +1,7 @@
-export default {
+const MediaQueries = {
   MOBILE_ONLY: `(max-width: 1279px)`,
 
   COMPUTER_ONLY: `(min-width: 1280px)`,
   COMPUTER_AT_LEAST: `(min-width: 1280px)`,
 };
+export default MediaQueries;
