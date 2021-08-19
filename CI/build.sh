956d6641a13550e167c49509d81d6083b1905d74
Paweł Kubrak <peku33@gmail.com>
2021-08-19 12:35:48 +0200

    ci: allow to specify example to build

--- CI/build.sh
+++ CI/build.sh
@@ -2,8 +2,21 @@
 
 set -e
 
+if [ -z "$1" ]
+then
+    echo "Usage: $0 example_name"
+    exit 1
+fi
+EXAMPLE_NAME="$1"
+
 CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=`realpath $CI_DIR/..`
+
+if [ ! -f "$ROOT_DIR/Controller/examples/test_$EXAMPLE_NAME.rs" ]
+then
+    echo "example $EXAMPLE_NAME does not exist"
+    exit 1
+fi
 
 WORKSPACE_DIR=$CI_DIR/Workspace
 mkdir -p $WORKSPACE_DIR
@@ -26,7 +39,7 @@ echo "Creating GUI Pack <"
 # Build Controller
 echo "Building Controller >"
 cd $ROOT_DIR/Controller
-CI_WEB_STATIC_PACK_GUI=$WEB_STATIC_PACK_GUI cargo build --release --locked --features ci --examples
+CI_WEB_STATIC_PACK_GUI=$WEB_STATIC_PACK_GUI cargo build --release --locked --features ci --example test_$EXAMPLE_NAME
 echo "Building Controller <"
 
 echo "All done!"
