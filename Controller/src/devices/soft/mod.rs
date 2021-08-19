75e3cf6929aecfb2af47148981972036090bc440
Paweł Kubrak <peku33@gmail.com>
2021-08-19 12:36:04 +0200

    feat: constant_a device

--- Controller/src/devices/soft/mod.rs
+++ Controller/src/devices/soft/mod.rs
@@ -2,4 +2,5 @@ pub mod debug;
 pub mod logic;
 pub mod surveillance;
 pub mod time;
+pub mod value;
 pub mod web;
---
+++ Controller/src/devices/soft/value/constant_a.rs
@@ -0,0 +1,72 @@
+use crate::{
+    devices,
+    signals::{self, signal, types::state::Value},
+    util::{
+        async_flag,
+        runtime::{Exited, Runnable},
+        waker_stream,
+    },
+};
+use async_trait::async_trait;
+use maplit::hashmap;
+use std::{any::type_name, borrow::Cow};
+
+#[derive(Debug)]
+pub struct Configuration<V: Value + Clone> {
+    pub value: V,
+}
+
+#[derive(Debug)]
+pub struct Device<V: Value + Clone> {
+    configuration: Configuration<V>,
+
+    signal_sources_changed_waker: waker_stream::mpsc::SenderReceiver,
+    signal_output: signal::state_source::Signal<V>,
+}
+impl<V: Value + Clone> Device<V> {
+    pub fn new(configuration: Configuration<V>) -> Self {
+        let value = configuration.value.clone();
+
+        Self {
+            configuration,
+
+            signal_sources_changed_waker: waker_stream::mpsc::SenderReceiver::new(),
+            signal_output: signal::state_source::Signal::new(Some(value)),
+        }
+    }
+}
+impl<V: Value + Clone> devices::Device for Device<V> {
+    fn class(&self) -> Cow<'static, str> {
+        Cow::from(format!("soft/value/constant_a<{}>", type_name::<V>()))
+    }
+
+    fn as_runnable(&self) -> &dyn Runnable {
+        self
+    }
+    fn as_signals_device(&self) -> &dyn signals::Device {
+        self
+    }
+}
+#[async_trait]
+impl<V: Value + Clone> Runnable for Device<V> {
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        exit_flag.await;
+        Exited
+    }
+}
+impl<V: Value + Clone> signals::Device for Device<V> {
+    fn signal_targets_changed_wake(&self) {
+        // no signal targets
+    }
+    fn signal_sources_changed_waker_receiver(&self) -> waker_stream::mpsc::ReceiverLease {
+        self.signal_sources_changed_waker.receiver()
+    }
+    fn signals(&self) -> signals::Signals {
+        hashmap! {
+            0 => &self.signal_output as &dyn signal::Base,
+        }
+    }
+}
