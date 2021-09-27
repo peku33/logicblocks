08b2b4d6979bae8c26873cf96dc5f7d4e8c2d766
Paweł Kubrak <peku33@gmail.com>
2021-09-27 21:52:38 +0200

    feat: GaugeLinear component for Ratio

---
+++ Gui/src/components/datatypes/ratio/GaugeLinear.tsx
@@ -0,0 +1,12 @@
+import GaugeLinearParent from "components/common/GaugeLinear";
+
+const GaugeLinear: React.FC<{
+  value: number;
+}> = (props) => {
+  return <GaugeLinearParent valueMin={0.0} valueMax={1.0} valueSerializer={valueSerializer} {...props} />;
+};
+export default GaugeLinear;
+
+export function valueSerializer(value: number): string {
+  return `${(value * 100).toFixed(0)}%`;
+}
