f9b8210ef08a292fd94dbce0d02802e776732968
Paweł Kubrak <peku33@gmail.com>
2022-04-03 19:15:07 +0200

    feat: ds18x20_to_temperature_a device

---
+++ Controller/src/devices/houseblocks/avr_v1/logic/devices/ds18x20_to_temperature_a.rs
@@ -0,0 +1,110 @@
+use super::super::super::datatypes::ds18x20::State as Ds18x20State;
+use crate::{
+    datatypes::temperature::Temperature,
+    devices,
+    signals::{self, signal},
+    util::{
+        async_ext::stream_take_until_exhausted::StreamTakeUntilExhaustedExt,
+        async_flag,
+        runtime::{Exited, Runnable},
+    },
+};
+use async_trait::async_trait;
+use futures::stream::StreamExt;
+use maplit::hashmap;
+use std::borrow::Cow;
+
+#[derive(Debug)]
+pub struct Device {
+    signals_targets_changed_waker: signals::waker::TargetsChangedWaker,
+    signals_sources_changed_waker: signals::waker::SourcesChangedWaker,
+    signal_input: signal::state_target_last::Signal<Ds18x20State>,
+    signal_output: signal::state_source::Signal<Temperature>,
+}
+impl Device {
+    pub fn new() -> Self {
+        Self {
+            signals_targets_changed_waker: signals::waker::TargetsChangedWaker::new(),
+            signals_sources_changed_waker: signals::waker::SourcesChangedWaker::new(),
+            signal_input: signal::state_target_last::Signal::<Ds18x20State>::new(),
+            signal_output: signal::state_source::Signal::<Temperature>::new(None),
+        }
+    }
+
+    fn signals_targets_changed(&self) {
+        let mut signal_sources_changed = false;
+
+        if let Some(signal_input) = self.signal_input.take_pending() {
+            let temperature = signal_input.and_then(|ds18x20_state| ds18x20_state.temperature);
+
+            if self.signal_output.set_one(temperature) {
+                signal_sources_changed = true;
+            }
+        }
+
+        if signal_sources_changed {
+            self.signals_sources_changed_waker.wake();
+        }
+    }
+
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.signals_targets_changed_waker
+            .stream(false)
+            .stream_take_until_exhausted(exit_flag)
+            .for_each(async move |()| {
+                self.signals_targets_changed();
+            })
+            .await;
+
+        Exited
+    }
+}
+
+impl devices::Device for Device {
+    fn class(&self) -> Cow<'static, str> {
+        Cow::from("houseblocks/avr_v1/logic/ds18x20_to_temperature_a")
+    }
+
+    fn as_runnable(&self) -> &dyn Runnable {
+        self
+    }
+    fn as_signals_device_base(&self) -> &dyn signals::DeviceBase {
+        self
+    }
+}
+
+#[async_trait]
+impl Runnable for Device {
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.run(exit_flag).await
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum SignalIdentifier {
+    Input,
+    Output,
+}
+impl signals::Identifier for SignalIdentifier {}
+impl signals::Device for Device {
+    fn targets_changed_waker(&self) -> Option<&signals::waker::TargetsChangedWaker> {
+        Some(&self.signals_targets_changed_waker)
+    }
+    fn sources_changed_waker(&self) -> Option<&signals::waker::SourcesChangedWaker> {
+        Some(&self.signals_sources_changed_waker)
+    }
+
+    type Identifier = SignalIdentifier;
+    fn by_identifier(&self) -> signals::ByIdentifier<Self::Identifier> {
+        hashmap! {
+            SignalIdentifier::Input => &self.signal_input as &dyn signal::Base,
+            SignalIdentifier::Output => &self.signal_output as &dyn signal::Base,
+        }
+    }
+}
