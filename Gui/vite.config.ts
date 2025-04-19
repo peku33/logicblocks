a98e495c27b83d8093f0beb676e57195f9c2ef87
Paweł Kubrak <peku33@gmail.com>
2025-04-19 10:36:33 +0200

    fix: manual chunking is broken

--- Gui/vite.config.ts
+++ Gui/vite.config.ts
@@ -1,41 +1,40 @@
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const directory_normalized = path.posix.basename(__dirname).replaceAll(path.sep, "/");
+// const directory_normalized = __dirname.replaceAll(path.sep, "/");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
+    sourcemap: true,
     chunkSizeWarningLimit: 10 * 1024 * 1024, // 10MB
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.startsWith("\x00")) {
-            return null;
-          }
+    // broken cyclic dependnecies: https://github.com/vitejs/vite/issues/12209
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id: string) {
+    //       if (id.startsWith("\x00")) {
+    //         return "builtin";
+    //       }
 
-          const module_path = path.posix.normalize(id);
-          const relative_path = path.posix.relative(directory_normalized, module_path);
+    //       const module_path = path.posix.normalize(id);
+    //       const relative_path = path.posix.relative(directory_normalized, module_path);
 
-          const path_items = relative_path.split("/");
+    //       const path_items = relative_path.split("/");
 
-          if (path_items[0] == "src") {
-            return "src";
-          }
-          if (path_items[0] == "node_modules") {
-            if (["@fortawesome"].includes(path_items[1])) {
-              return path_items[1];
-            }
+    //       if (path_items[0] == "node_modules") {
+    //         if (["@fortawesome", "react"].includes(path_items[1])) {
+    //           return path_items[1].replaceAll("@", "");
+    //         }
 
-            return "vendor";
-          }
+    //         return "vendor";
+    //       }
 
-          return null;
-        },
-      },
-    },
+    //       console.log({ id });
+    //       return "src";
+    //     },
+    //   },
+    // },
   },
 });
