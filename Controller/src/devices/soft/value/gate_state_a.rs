3cd2a88b9001fab7fbbf94d32332e00eefea7ddd
Paweł Kubrak <peku33@gmail.com>
2025-04-24 23:14:39 +0200

    feat: soft/value/gate_state_a device

---
+++ Controller/src/devices/soft/value/gate_state_a.rs
@@ -0,0 +1,131 @@
+use crate::{
+    devices,
+    signals::{self, signal, types::state::Value},
+    util::{
+        async_ext::stream_take_until_exhausted::StreamTakeUntilExhaustedExt,
+        async_flag,
+        runnable::{Exited, Runnable},
+    },
+};
+use async_trait::async_trait;
+use futures::stream::StreamExt;
+use maplit::hashmap;
+use std::{any::type_name, borrow::Cow};
+
+// When gate input is true, forwards input to output. When gate input is false
+// or not set - forwards None.
+#[derive(Debug)]
+pub struct Device<V>
+where
+    V: Value + Clone,
+{
+    signals_targets_changed_waker: signals::waker::TargetsChangedWaker,
+    signals_sources_changed_waker: signals::waker::SourcesChangedWaker,
+    signal_input: signal::state_target_last::Signal<V>,
+    signal_gate: signal::state_target_last::Signal<bool>,
+    signal_output: signal::state_source::Signal<V>,
+}
+impl<V> Device<V>
+where
+    V: Value + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            signals_targets_changed_waker: signals::waker::TargetsChangedWaker::new(),
+            signals_sources_changed_waker: signals::waker::SourcesChangedWaker::new(),
+            signal_input: signal::state_target_last::Signal::<V>::new(),
+            signal_gate: signal::state_target_last::Signal::<bool>::new(),
+            signal_output: signal::state_source::Signal::<V>::new(None),
+        }
+    }
+
+    fn calculate(
+        input: Option<V>,
+        gate: Option<bool>,
+    ) -> Option<V> {
+        if gate == Some(true) { input } else { None }
+    }
+
+    fn signals_targets_changed(&self) {
+        let output = Self::calculate(
+            self.signal_input.take_last().value,
+            self.signal_gate.take_last().value,
+        );
+
+        if self.signal_output.set_one(output) {
+            self.signals_sources_changed_waker.wake();
+        }
+    }
+
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.signals_targets_changed_waker
+            .stream()
+            .stream_take_until_exhausted(exit_flag)
+            .for_each(async |()| {
+                self.signals_targets_changed();
+            })
+            .await;
+
+        Exited
+    }
+}
+
+impl<V> devices::Device for Device<V>
+where
+    V: Value + Clone,
+{
+    fn class(&self) -> Cow<'static, str> {
+        Cow::from(format!("soft/value/gate_state_a<{}>", type_name::<V>()))
+    }
+
+    fn as_runnable(&self) -> &dyn Runnable {
+        self
+    }
+    fn as_signals_device_base(&self) -> &dyn signals::DeviceBase {
+        self
+    }
+}
+
+#[async_trait]
+impl<V> Runnable for Device<V>
+where
+    V: Value + Clone,
+{
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.run(exit_flag).await
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum SignalIdentifier {
+    Input,
+    Gate,
+    Output,
+}
+impl signals::Identifier for SignalIdentifier {}
+impl<V> signals::Device for Device<V>
+where
+    V: Value + Clone,
+{
+    fn targets_changed_waker(&self) -> Option<&signals::waker::TargetsChangedWaker> {
+        Some(&self.signals_targets_changed_waker)
+    }
+    fn sources_changed_waker(&self) -> Option<&signals::waker::SourcesChangedWaker> {
+        Some(&self.signals_sources_changed_waker)
+    }
+
+    type Identifier = SignalIdentifier;
+    fn by_identifier(&self) -> signals::ByIdentifier<Self::Identifier> {
+        hashmap! {
+            SignalIdentifier::Input => &self.signal_input as &dyn signal::Base,
+            SignalIdentifier::Gate => &self.signal_gate as &dyn signal::Base,
+            SignalIdentifier::Output => &self.signal_output as &dyn signal::Base,
+        }
+    }
+}
--- Controller/src/devices/soft/value/mod.rs
+++ Controller/src/devices/soft/value/mod.rs
@@ -2,6 +2,7 @@ pub mod broadcast_event_a;
 pub mod broadcast_state_a;
 pub mod coalesce_a;
 pub mod constant_a;
+pub mod gate_state_a;
 pub mod latch_a;
 pub mod sample_a;
 pub mod trigger_a;
