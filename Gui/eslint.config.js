43b8305d066702dbe7870c44bdb2de5b077836d3
Paweł Kubrak <peku33@gmail.com>
2026-02-26 00:11:30 +0100

    chore: updated deps

--- Gui/eslint.config.js
+++ Gui/eslint.config.js
@@ -1,13 +1,16 @@
 import eslint from "@eslint/js";
 import prettier from "eslint-plugin-prettier/recommended";
 import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+// import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
 // import reactRefresh from "eslint-plugin-react-refresh";
 import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+// TODO: reenable rect when it becomes compatible with eslint v10
+
+export default defineConfig(
   {
     ignores: ["dist"],
   },
@@ -18,9 +21,9 @@ export default tseslint.config(
       eslint.configs.recommended,
       tseslint.configs.strictTypeChecked,
       tseslint.configs.stylisticTypeChecked,
-      react.configs.flat.recommended,
-      react.configs.flat["jsx-runtime"],
-      reactHooks.configs["recommended-latest"],
+      // react.configs.flat.recommended,
+      // react.configs.flat["jsx-runtime"],
+      // reactHooks.configs["recommended-latest"],
       // reactRefresh.configs.vite,
       prettier,
     ],
