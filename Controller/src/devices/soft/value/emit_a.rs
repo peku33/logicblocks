e610d17c383cae5857244e0b77fed0bcad81db21
Paweł Kubrak <peku33@gmail.com>
2026-08-11 21:45:42 +0200

    feat: add emit_a

---
+++ Controller/src/devices/soft/value/emit_a.rs
@@ -0,0 +1,136 @@
+use crate::{
+    devices,
+    signals::{self, signal, types::event::Value},
+    util::{
+        async_ext::stream_take_until_exhausted::StreamTakeUntilExhaustedExt,
+        async_flag,
+        runnable::{Exited, Runnable},
+    },
+};
+use async_trait::async_trait;
+use futures::stream::StreamExt;
+use maplit::hashmap;
+use std::{any::type_name, borrow::Cow};
+
+#[derive(Debug)]
+pub struct Configuration<V>
+where
+    V: Value + Clone,
+{
+    /// value emitted on every trigger
+    pub value: V,
+}
+
+/// when Event<()> hits trigger target
+/// configured fixed value is emitted as Event<V> from output source
+#[derive(Debug)]
+pub struct Device<V>
+where
+    V: Value + Clone,
+{
+    configuration: Configuration<V>,
+
+    signals_targets_changed_waker: signals::waker::TargetsChangedWaker,
+    signals_sources_changed_waker: signals::waker::SourcesChangedWaker,
+    signal_trigger: signal::event_target_last::Signal<()>,
+    signal_output: signal::event_source::Signal<V>,
+}
+impl<V> Device<V>
+where
+    V: Value + Clone,
+{
+    pub fn new(configuration: Configuration<V>) -> Self {
+        Self {
+            configuration,
+
+            signals_targets_changed_waker: signals::waker::TargetsChangedWaker::new(),
+            signals_sources_changed_waker: signals::waker::SourcesChangedWaker::new(),
+            signal_trigger: signal::event_target_last::Signal::<()>::new(),
+            signal_output: signal::event_source::Signal::<V>::new(),
+        }
+    }
+
+    fn signals_targets_changed(&self) {
+        // act only if trigger was fired
+        match self.signal_trigger.take_pending() {
+            Some(()) => {}
+            None => return,
+        }
+
+        let output = self.configuration.value.clone();
+
+        if self.signal_output.push_one(output) {
+            self.signals_sources_changed_waker.wake();
+        }
+    }
+
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.signals_targets_changed_waker
+            .stream()
+            .stream_take_until_exhausted(exit_flag)
+            .for_each(async |()| {
+                self.signals_targets_changed();
+            })
+            .await;
+
+        Exited
+    }
+}
+
+impl<V> devices::Device for Device<V>
+where
+    V: Value + Clone,
+{
+    fn class(&self) -> Cow<'static, str> {
+        Cow::from(format!("soft/value/emit_a<{}>", type_name::<V>()))
+    }
+
+    fn as_runnable(&self) -> &dyn Runnable {
+        self
+    }
+    fn as_signals_device_base(&self) -> &dyn signals::DeviceBase {
+        self
+    }
+}
+
+#[async_trait]
+impl<V> Runnable for Device<V>
+where
+    V: Value + Clone,
+{
+    async fn run(
+        &self,
+        exit_flag: async_flag::Receiver,
+    ) -> Exited {
+        self.run(exit_flag).await
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum SignalIdentifier {
+    Trigger,
+    Output,
+}
+impl signals::Identifier for SignalIdentifier {}
+impl<V> signals::Device for Device<V>
+where
+    V: Value + Clone,
+{
+    fn targets_changed_waker(&self) -> Option<&signals::waker::TargetsChangedWaker> {
+        Some(&self.signals_targets_changed_waker)
+    }
+    fn sources_changed_waker(&self) -> Option<&signals::waker::SourcesChangedWaker> {
+        Some(&self.signals_sources_changed_waker)
+    }
+
+    type Identifier = SignalIdentifier;
+    fn by_identifier(&self) -> signals::ByIdentifier<'_, Self::Identifier> {
+        hashmap! {
+            SignalIdentifier::Trigger => &self.signal_trigger as &dyn signal::Base,
+            SignalIdentifier::Output => &self.signal_output as &dyn signal::Base,
+        }
+    }
+}
